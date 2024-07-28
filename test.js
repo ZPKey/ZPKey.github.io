047721bb5b07ff4c71dd5f711426d5ba64f18ea9
Z.P <zperacha14@gmail.com>
2024-07-28 18:23:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,78 +1,44 @@
 // Convention: https://bl.ocks.org/mbostock/3019563
-const margin = { top: 10, right: 50, bottom: 50, left: 50 },
-  width = 450 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 1000 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
 
-const svg = d3.select("#root").attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+// append the svg object to the body of the page
+var svg = d3.select("#root")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-// Define chart area
-svg
-  .append("clipPath")
-  .attr("id", "chart-area")
-  .append("rect")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("width", width)
-  .attr("height", height)
+//Read the data
+d3.csv("https://flunky.github.io/cars2017.csv", function(data) {
 
-// Add Axes
-const xMax = 4;
-const yMax = 5;
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 200])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-let xScale = d3.scaleLinear([0, xMax], [0, width])
-let yScale = d3.scaleLinear([0, yMax], [height, 0])
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 12])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-let xAxis = d3.axisBottom(xScale)
-let yAxis = d3.axisLeft(yScale)
-svg.append("g")
-  .attr("transform", `translate(0,${height})`)
-  .call(xAxis)
-svg.append("g")
-  .attr("transform", `translate(0,0)`)
-  .call(yAxis)
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.AverageCityMPG); } )
+      .attr("cy", function (d) { return y(d.EngineCylinders); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
 
-// Axes label
-svg.append("text")
-  .attr("class", "x label")
-  .attr("text-anchor", "end")
-  .attr("x", width / 2 + 5)
-  .attr("y", height + 35)
-  .text("x");
-
-svg.append("text")
-  .attr("class", "y label")
-  .attr("text-anchor", "end")
-  .attr("y", -35)
-  .attr("x", -height / 2)
-  .attr("transform", "rotate(-90)")
-  .html("y")
-
-function f(x) {
-  return x * 2 + 1;
-}
-
-function graphFunction() {
-  pointNum = 500;
-
-  const data = [];
-  for (let x = 0; x <= pointNum; x++) {
-    y = f(x);
-    data.push([x, y])
-  }
-  return data;
-}
-
-// Add function graph
-let line = d3.line()
-  .x(d => xScale(d[0]))
-  .y(d => yScale(d[1]))
-svg.append("path")
-  .datum(graphFunction())
-  .attr("clip-path", "url(#chart-area)")
-  .attr("fill", "none")
-  .attr("stroke", "teal")
-  .attr("stroke-width", 2)
-  .attr("d", line); 
+})
